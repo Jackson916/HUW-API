0f02b81eaf8ca10d441093bd63f65f467cc7b41f
jackson916 <colingrey412@gmail.com>
2022-05-23 12:07:22 -0400

    creadted list routes

---
+++ app/routes/list_routes.js
@@ -0,0 +1,115 @@
+// Express docs: http://expressjs.com/en/api.html
+const express = require('express')
+// Passport docs: http://www.passportjs.org/docs/
+const passport = require('passport')
+
+// pull in Mongoose model for examples
+const List = require('../models/list')
+
+// this is a collection of methods that help us detect situations when we need
+// to throw a custom error
+const customErrors = require('../../lib/custom_errors')
+
+// we'll use this function to send 404 when non-existant document is requested
+const handle404 = customErrors.handle404
+// we'll use this function to send 401 when a user tries to modify a resource
+// that's owned by someone else
+const requireOwnership = customErrors.requireOwnership
+
+// this is middleware that will remove blank fields from `req.body`, e.g.
+// { example: { title: '', text: 'foo' } } -> { example: { text: 'foo' } }
+const removeBlanks = require('../../lib/remove_blank_fields')
+// passing this as a second argument to `router.<verb>` will make it
+// so that a token MUST be passed for that route to be available
+// it will also set `req.user`
+const requireToken = passport.authenticate('bearer', { session: false })
+
+// instantiate a router (mini app that only handles routes)
+const router = express.Router()
+
+// INDEX
+// GET /examples
+router.get('/list', requireToken, (req, res, next) => {
+  List.find()
+    .then(list => {
+      // `examples` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return list.map(list => list.toObject())
+    })
+    // respond with status 200 and JSON of the examples
+    .then(list => res.status(200).json({ list: list }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// SHOW
+// GET /examples/5a7db6c74d55bc51bdf39793
+router.get('/list/:id', requireToken, (req, res, next) => {
+  // req.params.id will be set based on the `:id` in the route
+  List.findById(req.params.id)
+    .then(handle404)
+    // if `findById` is succesful, respond with 200 and "example" JSON
+    .then(list => res.status(200).json({ list: list.toObject() }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// CREATE
+// POST /examples
+router.post('/list', requireToken, (req, res, next) => {
+  // set owner of new example to be current user
+  req.body.list.owner = req.user.id
+
+  List.create(req.body.list)
+    // respond to succesful `create` with status 201 and JSON of new "example"
+    .then(list => {
+      res.status(201).json({ list: list.toObject() })
+    })
+    // if an error occurs, pass it off to our error handler
+    // the error handler needs the error message and the `res` object so that it
+    // can send an error message back to the client
+    .catch(next)
+})
+
+// UPDATE
+// PATCH /examples/5a7db6c74d55bc51bdf39793
+router.patch('/list/:id', requireToken, removeBlanks, (req, res, next) => {
+  // if the client attempts to change the `owner` property by including a new
+  // owner, prevent that by deleting that key/value pair
+  delete req.body.list.owner
+
+  List.findById(req.params.id)
+    .then(handle404)
+    .then(list => {
+      // pass the `req` object and the Mongoose record to `requireOwnership`
+      // it will throw an error if the current user isn't the owner
+      requireOwnership(req, List)
+
+      // pass the result of Mongoose's `.update` to the next `.then`
+      return List.updateOne(req.body.list)
+    })
+    // if that succeeded, return 204 and no JSON
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// DESTROY
+// DELETE /examples/5a7db6c74d55bc51bdf39793
+router.delete('/list/:id', requireToken, (req, res, next) => {
+  List.findById(req.params.id)
+    .then(handle404)
+    .then(list => {
+      // throw an error if current user doesn't own `example`
+      requireOwnership(req, list)
+      // delete the example ONLY IF the above didn't throw
+      List.deleteOne()
+    })
+    // send back 204 and no content if the deletion succeeded
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+module.exports = router
